9988d94b6aaa9cd318363bd5f4c17fdd0920ce9e
TomasIbaceta <33475042+TomasIbaceta@users.noreply.github.com>
2020-07-06 00:58:12 -0400

    update separacion
    ahora está todo en archivos distintos

---
+++ SSOO_tarea3/queue.c
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "queue.h"
+#include <stdio.h>
+void init(queue_t* q)
+{
+    for (int i=0; i<(QUEUE_MAX_SIZE); i++){
+        q->queue[i] = 0;
+    }
+    
+    q->head = q->queue;
+    q->tail = q->queue;
+    
+    
+}
+
+int enqueue(queue_t* q, int element){
+    if ( isFull(q)){
+        printf("ERROR: queue full\n");
+        return -1;
+    }
+
+    else{
+        printf("Element Queued: %d\n", element);
+        *q->tail = element;
+        if ( !isFull(q) ){
+            q->tail++;
+        }
+        return 0;  
+    }
+}
+
+int dequeue(queue_t* q){ //retornar el primer elemento, shiftear todos uno para la izquierda
+    if ( isEmpty(q) ){
+        printf("ERROR: queue is empty\n");
+        return -1;
+    }
+    
+    else{
+        int dequeued = *(q->head);
+    
+        for (int i = 0; q->head+i != q->tail; i++){ //shiftea todos uno a la izquierda, manteniendo las posiciones de head y tail
+            *(q->head+i) = *(q->head+i+1);
+        }
+        *(q->tail--) = 0;
+
+        printf("Element Dequeued: %d\n", dequeued);
+        return dequeued;
+    }
+}
+
+int isFull(queue_t* q){
+    return (q->tail) == &(q->queue[QUEUE_MAX_SIZE]);
+}
+
+int isEmpty(queue_t* q){
+    return q->head == q->tail;
+}
---
+++ SSOO_tarea3/queue.h
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   queue.h
+ * Author: toman
+ *
+ * Created on July 6, 2020, 12:32 AM
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define QUEUE_MAX_SIZE 5
+
+typedef struct queue_t{
+    int queue[QUEUE_MAX_SIZE+1];
+    int *head;
+    int *tail;
+}queue_t;
+
+void init(queue_t*);
+int enqueue(queue_t*, int); //return int para corrección de errores
+int dequeue(queue_t*);
+int isFull(queue_t*);
+int isEmpty(queue_t*);    
+    
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* QUEUE_H */
+
